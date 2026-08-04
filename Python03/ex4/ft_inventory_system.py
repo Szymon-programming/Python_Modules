28744602aedb153232fe80ac70b02b1c541a0d83
Szymon Choiński <choinskiszymon123@gmail.com>
2026-08-04 22:51:23 +0200

    working error_handling

--- Python03/ex4/ft_inventory_system.py
+++ Python03/ex4/ft_inventory_system.py
@@ -5,9 +5,20 @@ def main() -> None:
     print("=== Inventory System Analysis ===")
     inventory = {}
     for argument in sys.argv[1:]:
-        if ":" in argument:
-            item, quantity = argument.split(":", 1)
-            inventory[item] = int(quantity)
+        try:
+			if ":" in argument:
+				item, quantity = argument.split(":", 1)
+			else:
+				raise SyntaxError
+			if item in inventory:
+				raise KeyError
+			inventory[item] = int(quantity)
+		except KeyError:
+			print(f"Redundant item '{item}' - discarding")
+		except ValueError as e:
+			print(f"Quantity error for 'key': {e}")
+		except SyntaxError:
+			print(f"Error - invalid parameter '{argument}'")
     print(f"got inventory: {inventory}")
     item_list = list(inventory.keys())
     print(f"Item list: {item_list}")
@@ -24,10 +35,10 @@ def main() -> None:
         print(f"Item {item} represents {item_quantity}%")
     most_abundant = max(inventory, key=lambda quantity: inventory[quantity])
     print(f"Item most abundant: {most_abundant} with"
-          f"quantity {inventory[most_abundant]}")
+          f" quantity {inventory[most_abundant]}")
     least_abundant = min(inventory, key=lambda quantity: inventory[quantity])
     print(f"Item least abundant: {least_abundant} with"
-          f"quantity {inventory[least_abundant]}")
+          f" quantity {inventory[least_abundant]}")
     inventory.update(magic_item=1)
     print(f"Updated inventory: {inventory}")
 
